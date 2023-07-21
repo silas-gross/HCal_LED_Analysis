5d7bb88dcd9003d9ea675aa266356acbe51b0d75
Nikhil Kumar <nkumar7252@gmail.com>
2023-07-21 13:04:54 -0400

    Update CompareLEDRuns.h

---
+++ CompareLEDRuns.h
@@ -0,0 +1,24 @@
+#ifndef __COMPARELEDRUNS_H__
+#define __COMPARELEDRUNS_H__
+
+#include <pmonitor/pmonitor.h>
+#include <Event/Event.h>
+#include <Event/EventTypes.h>
+#include <vector>
+#include <map>
+#include <string>
+
+class CompareLEDRuns
+{
+    private:
+    //private variables
+        //struct
+
+    public: 
+    //public variables, get and set methods etc.
+
+
+
+}
+
+#endif
