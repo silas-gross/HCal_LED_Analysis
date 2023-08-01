e31cd7bc512195ce141baf6cf280d8d2c0fe5567
Silas Grossberndt <silas.grossberndt@gmail.com>
2023-08-01 13:20:33 -0400

    built base model waveform processor A\*, just doing linear interprutation point by point and mimimizing chi^2/ndf

--- GetLEDData.cc
+++ GetLEDData.cc
@@ -1,7 +1,38 @@
 #include "GetLEDData.h"
 #include <TFile.h>
 #include <TString.h>
-
+LEDRunDtaa::Heuristic(std::vector<int> data, std::vector<int> wf, int ndf)
+{
+	//use chi_squared/ndf as a fitting heuristic
+	float chi=0;
+	for(int i=0; i<data.size; i++) chi+=pow(data[i]-wf[i],2)/data[i];
+	chi=chi/ndf; 
+	return chi;
+}
+LEDRunData::FindWaveForm(std::vector*<int> chl_data, int pos)
+{
+	int n_params=1; 
+	std::vector<int> model (chl_data->size, chl_data->at(pos)), data=*chl_data; //model of just peak value
+	std::vector<float>param {data[pos]};
+	float heuristic=Heuristic(data, model, n_params);
+	std::map<float, std::pair<std::vector<float>, std::vector<int>>> child_models; //this will be used to generate the queue
+	for(int i=0; i<data.size; i++) //generate the first set of children, linear models
+	{
+		if(i==pos) continue;
+		std::vector<float> params {data[pos], data[i]};
+		float slope=(data[pos]-data[i])/(pos-i);
+		float ints=data[pos]-slope*i;
+		std::vector<int>temp_model; 
+		for(int j=0; j<data.size; j++){
+		       float mp=slope*j+ints;
+		       temp_model.push_back((int)mp);
+		}
+		float temp_heur=Heutristic(data, temp_model, params.size);
+		child_models[temp_heur]=std::make_pair(params, temp_model);
+	}	
+	//For the next set, use function pointer to generate the heuristic
+	
+}
 LEDRunData::getPedestal(std::vector<int> chl_data) //just gets the pedestal value from first 3 samples
 {
 	int pv, d1=chl_data[0], d2=chl_data[1], d3=chl_data[2];
@@ -12,12 +43,24 @@ LEDRunData::getPedestal(std::vector<int> chl_data) //just gets the pedestal valu
 LEDRunData::getPeak(std::vector<int> chl_data, int pedestal) //gets peak value, peak position, peak width, pedestal rms
 {
 	std::vector<float> peak_data;
-	float full_val, pos, width, rms;
-	for(int sp:chl_data) full_val+=(sp-pedestal);
+	float full_val=0, pos=0, width=0, rms=0, pk=0; 
+	for(int sp=0; sp<chl_data.size; sp++){
+		full_val+=(chl_data[sp]-pedestal);
+		if(chl_data[sp]>pk){
+			pk=chl_data[sp];
+			pos=sp+1;
+		}
+	}
 	peak_data.push_back(full_val);
+	peak_data.push_back(pos);
 	//now need to do waveform fitting, just going to do a very quick a* search
-	
-}
+	width=FindWaveForm(&chl_data, (int)pos);
+	peak_data.push_back(width);
+	for(int sp:chl_data) rms+=pow(sp-pedestal, 2);
+	rms=sqrt(1/(chl_data.size)*rms);
+	peak_data.push_back(rms);
+	return peak_data;
+}	
 LEDRunData::process_event(Event *e){
 	
 	for(auto pid:packets){
@@ -34,8 +77,11 @@ LEDRunData::process_event(Event *e){
 		std::vector<float>pk_data=getPeak(channel_data, pedestal);
 		std::pair<int, int> location {p,c};
 		datahists[location][0]->Fill(evtval); 
-		datahists[location][1]->Fill(pedestal);
-		datahists[location][2]->Fill(
+		datahists[location][1]->Fill(pk[0]);
+		datahists[location][2]->Fill(pedestal);
+		datahists[location][3]->Fill(pk[3]);
+		datahists[location][4]->Fill(pk[1]);
+		datahists[location][5]->Fill(pk[2]);
 	       }
 	}	       
 }
