ac9e29d4af933210949f59ec7eb85e00c5d1576b
Nikhil Kumar <nkumar7252@gmail.com>
2023-08-03 18:34:01 -0400

    more work for peak amp, mean, sigma
    peak, mean and sigma are now plotted for ihcal and ohcal. added a pad for ihcal and ohcal.

--- CompareLEDRuns.cc
+++ CompareLEDRuns.cc
@@ -5,19 +5,27 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <map>
 #include <TFile.h>
 #include <TString.h>
 #include <TH1.h>
-#include <TH1F.h>//maybe not needed.
+#include <TH1F.h>//a lot of these maybe not needed.
 #include <TH2F.h>
 #include <TF1.h>
 #include <TF2.h>
 #include <TCanvas.h>
 #include <TGraph.h>
+#include <TPad.h>
+#include <TText.h>
+#include <TStyle.h>
+#include <TRoot.h>
+
+
 // I need to characterize: Ped, peak, ped_rms, peak location, peak width
-//structures here for now. I can move it later.
+
+//structure definitions
 struct GaussFitResult {
     //custom structure to hold fit parameter data.
     double mean;
@@ -32,17 +40,18 @@ struct DateRunBeam {
     bool Beam; //No->0, Yes->1
 };
 
-//function declarations//written like this to start. Can be moved later
-GaussFitResult Hist_Fit_1D(const char* filename, TH1F* histogram);
-float Tower_Slope_Fit(vector<float> TowerPeaks);
+//function declarations
+GaussFitResult Hist_Fit_1D(const char* filename, const char* histogramName);// pass a filename and a TH1F histname and recive gaussian fit parameters
+float Tower_Slope_Fit(vector<float> TowerPeaks);// pass a vector of peak values for a single tower over many runs and recieve the  value vs time slope
+std::vector<TGraph*> CreateTGraphVector(const std::vector<DateRunBeam>& run_info, const char* histogramName); // pass a vector of all run_info (defined in the csv) and make tgraphs of gaussian sigma, mean, amp using Hist_Fit_1D
 
 //function definitions
-GaussFitResult Hist_Fit_1D(const char* filename, TH1F* histogram){
+GaussFitResult Hist_Fit_1D(const char* filename, const char* histogramName){
     //open the root file called filename.root
     TFile *f = new TFile(Form("run_%s.root", filename));
     // pull up relevant histograms
     //TH1F *htemp=(TH1F*)f->Get(Form("%s", histogram));// c style cast, should work but I will try something else
-     TH1F* htemp = static_cast<TH1F*>(f->Get(histogram->GetName()));//c++ style cast
+     TH1F* htemp = static_cast<TH1F*>(f->Get(histogramName));//c++ style cast
     //Fit the two histograms
     htemp->Fit("gaus");
     //create calls to the fits
@@ -77,6 +86,55 @@ float Tower_Slope_Fit(vector<float> TowerPeaks);{
     return slope;
 }
 
+std::vector<TGraph*> CreateTGraphVector(const std::vector<DateRunBeam>& run_info, const char* histogramName){
+    std::vector<TGraph*> graphVector;
+    // Extract data from the vectors and create arrays
+    int numRuns = run_info.size();
+    double* Run_Number = new double[numRuns];
+    double* Run_mean = new double[numRuns];
+    double* Run_sigma = new double[numRuns];
+    double* Run_amp = new double[numRuns];
+    //-------------------------process run data
+    vector<GaussFitResult> GaussFitParam;
+    for (int i=0; i<numRuns; i++){//
+        GaussFitParam.push_back(Hist_Fit_1D(run_info[i].run_number.c_str(), histogramName)); //execute peak fit for the specific run number and histogramand store results in vector
+        Run_Number[i]=std::stoi(run_info[i].run_number);
+        Run_mean[i]=GaussFitParam[i].mean;
+        Run_sigma[i]=GaussFitParam[i].sigma;
+        Run_amp[i]=GaussFitParam[i].amp;  
+    }
+    // Create graphs to plot the fit parameters against the run numbers
+    TGraph* tgraph1 = new TGraph(numRuns, &Run_Number[0], &Run_mean[0]);
+    graph1->SetTitle("Mean Value vs Run Number");
+    graph1->GetXaxis()->SetTitle("Run Number");
+    graph1->GetYaxis()->SetTitle("Mean Value");
+    //graph1->Draw("AP"); // Draw the graph as points 'P', Draw and scale axis 'A'
+
+    TGraph* tgraph2 = new TGraph(numRuns, &Run_Number[0], &Run_sigma[0]);
+    graph2->SetTitle("Sigma vs Run Number");
+    graph2->GetXaxis()->SetTitle("Run Number");
+    graph2->GetYaxis()->SetTitle("Sigma");
+    //graph2->Draw("AP");  
+
+    TGraph* tgraph3 = new TGraph(numRuns, &Run_Number[0], &Run_amp[0]);
+    graph3->SetTitle("Amplitude vs Run Number");
+    graph3->GetXaxis()->SetTitle("Run Number");
+    graph3->GetYaxis()->SetTitle("Amplitude");
+    //graph3->Draw("AP");
+    //store graphs in vector
+    graphVector.push_back(tgraph1);
+    graphVector.push_back(tgraph2);
+    graphVector.push_back(tgraph3);
+    //delete dynamically assigned memory
+    delete[] Run_Number;
+    delete[] Run_mean;
+    delete[] Run_sigma;
+    delete[] Run_amp;
+    //return graphs
+    return graphVector;
+} 
+
+//main program
 int main{
     //--------------------------histograms
 
@@ -119,51 +177,90 @@ int main{
             std::cout << "Error: Unexpected Beam Status" << std::endl;
         return 1;
         }
-        run_info.push_back({row[0],row[1],Beam_on});
+        run_info.push_back({row[0], row[1], Beam_on});
         //move on to next row/line (each specific led run)
     }
-    // std::cout << " Filler" << ;
+    const char* ohcalhistname="h_peak_ohcal";
+    const char* ihcalhistname="h_peak_ihcal";
+    std::vector<TGraph*> gauss_Peak_graphs_ohcal = CreateTGraphVector(run_info, ohcalhistname);
+    std::vector<TGraph*> gauss_Peak_graphs_ihcal = CreateTGraphVector(run_info, ihcalhistname);
+    // Create a TCanvas to draw the TGraphs
+    TCanvas* c1 = new TCanvas("canvas1", "Peak Parameter graphs", 1800, 1500);
+    c1->Divide(1,2);//divide in to two pads. one for ohcal. one for ihcal
+    // columns, rows// need 3 columns each for ohcal and ihcal
+    //create a vector of tgraphs for sigma, mean, amp
+    
+    /* // maybe I can clean this up and do it in a loop later
+    for (size_t i = 0; i < myGraphs.size(); ++i) {
+        canvas->cd(i+1); // Activate the canvas
+        CreateTGraphVector[i]->Draw((i == 0) ? "APL" : "PL"); // Use "APL" for the first graph, "PL" for the rest
+    }*/
 
-    // Extract data from the vectors and create arrays
-    int numRuns = run_info.size();
-    double* Run_Number = new double[numRuns];
-    double* Run_mean = new double[numRuns];
-    double* Run_sigma = new double[numRuns];
-    double* Run_amp = new double[numRuns];
-    //-------------------------process run data
-    vector<GaussFitResult> GaussFitParam;
-    for (int i=0; i<numRuns; i++){//
-        GaussFitParam.push_back(Hist_Fit_1D(run_info[i,1].c_str())); //execute peak fit for the specific run number and store results in vector
-        Run_Number[i]=std::stoi(run_info[i].run_number);
-        Run_mean[i]=run_info[i].mean_ih;
-        Run_sigma[i]=run_info[i].sigma_ih;
-        Run_amp[i]=run_info[i].amplitude_ih;
-    }
-    // Create graphs to plot the fit parameters against the run numbers
-    TGraph* tgraph1 = new TGraph(numRuns, &Run_Number[0], &Run_mean[0]);
-    TGraph* tgraph2 = new TGraph(numRuns, &Run_Number[0], &Run_sigma[0]);
-    TGraph* tgraph3 = new TGraph(numRuns, &Run_Number[0], &Run_amp[0]);
-    // Create a canvas to display the graph
-    TCanvas* canvas1 = new TCanvas("canvas1", "Fit Parameters vs Run Number", 800, 600);
-    graph1->SetTitle("Mean Value vs Run Number");
-    graph1->GetXaxis()->SetTitle("Run Number");
-    graph1->GetYaxis()->SetTitle("Mean Value");
-    graph2->SetTitle("Sigma vs Run Number");
-    graph2->GetXaxis()->SetTitle("Run Number");
-    graph2->GetYaxis()->SetTitle("Sigma");
-    graph3->SetTitle("Amplitude vs Run Number");
-    graph3->GetXaxis()->SetTitle("Run Number");
-    graph3->GetYaxis()->SetTitle("Amplitude");
-    graph1->Draw("AP"); // Draw the graph as points 'P', Draw and scale axis 'A'
-    graph2->Draw("AP");  
-    graph3->Draw("AP");
-
+    //-------------------------------------------
+    //top pad will be ihcal
+    TPad* TopPad = (TPad*)c1->cd(1);
+    //divide in to 3 columns for peak, sigma, amp
+    TopPad->Divide(3,1);
+    //ihcal peak mean
+    TopPad->cd(1);
+    gPad->SetTopMargin(0.12);
+    gPad->SetFillColor(33);
+    gauss_Peak_graphs_ihcal[1]->Draw("A");
+    //ihcal peak sigma
+    TopPad->cd(2);
+    gPad->SetTopMargin(0.12);
+    gPad->SetLeftMargin(0.15);
+    gPad->SetFillColor(33);
+    gauss_Peak_graphs_ihcal[2]->Draw("A");
+    //ihcal peak amplitude
+    TopPad->cd(3);
+    gPad->SetTopMargin(0.12);
+    gPad->SetLeftMargin(0.15);
+    gPad->SetFillColor(33);
+    gauss_Peak_graphs_ihcal[3]->Draw("A");
+    
+    //-----------------------------------------
+    //bottom pad is ohcal
+    TPad* BotPad = (TPad*)c1->cd(2);
+    //divide in to 3 columns for peak, sigma, amp
+    BotPad->Divide(3,1);
+    //ihcal peak mean
+    BotPad->cd(1);
+    gPad->SetTopMargin(0.12);
+    gPad->SetFillColor(33);
+    gauss_Peak_graphs_ihcal[1]->Draw("A");
+    //ihcal peak sigma
+    BotPad->cd(2);
+    gPad->SetTopMargin(0.12);
+    gPad->SetLeftMargin(0.15);
+    gPad->SetFillColor(33);
+    gauss_Peak_graphs_ihcal[2]->Draw("A");
+    //ihcal peak amplitude
+    BotPad->cd(3);
+    gPad->SetTopMargin(0.12);
+    gPad->SetLeftMargin(0.15);
+    gPad->SetFillColor(33);
+    gauss_Peak_graphs_ihcal[3]->Draw("A");
+    
+    //-------------------------------------------
     // Keep the canvas open to display the graph
-    //canvas->Update();
+    c1->Update();
     //canvas->Modified();
-    //canvas->WaitPrimitive();
+    c1->WaitPrimitive();//wait for the user to close the program
+    //*
     
 
+    //clean up dynamically allocated memory
+    for (const auto& graph : gauss_Peak_graph_ohcal) {
+        delete graph;
+    }
+    for (const auto& graph : gauss_Peak_graph_ihcal) {
+        delete graph;
+    }
+    delete canvas1;
+    delete ohcalhistname;
+    delete ihcalhistname;
+    //*/
     file.close();
     return 1;
 }
